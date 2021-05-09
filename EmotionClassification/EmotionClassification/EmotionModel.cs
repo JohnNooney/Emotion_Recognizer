79329c4f7f3842d9c052cc983b4ccdf21960832f
John Nooney <1803534@uad.ac.uk>
2021-05-09 20:40:30 +0100

    Add more dialogue options as well as ability to mass predict images

--- EmotionClassification/EmotionClassification/EmotionModel.cs
+++ EmotionClassification/EmotionClassification/EmotionModel.cs
@@ -12,22 +12,26 @@ namespace EmotionClassification
         private DataOperationsCatalog.TrainTestData split;
         private DataViewSchema modelSchema; //Define DataViewSchema for data preparation pipeline and trained model
         private ITransformer model;
+        private string csv;
 
-        public EmotionModel()
+        public EmotionModel(string _csv)
         {
             mlContext = new MLContext();
-        }
 
-        public void TrainModel(string csv)
-        {
-            //create instance to train model
-            //var mlContext = new MLContext();
+            csv = _csv;
 
             //load data into DataView
             IDataView testDataView = mlContext.Data.LoadFromTextFile<FaceData>(csv, hasHeader: true, separatorChar: ',');
 
             //split the training data by 80/20
             split = mlContext.Data.TrainTestSplit(testDataView, testFraction: 0.2);
+        }
+
+        public void TrainModel()
+        {
+            //create instance to train model
+            //var mlContext = new MLContext();
+
 
             //define models pipeline
             var featureVectorName = "Features";
@@ -52,8 +56,10 @@ namespace EmotionClassification
             //fit the model (train)
             model = pipeline.Fit(split.TrainSet);
 
-            //cross validation that can be used to tune the hyperparameters. These are responsible for controling the learning process
-            var crossValidationResults = mlContext.MulticlassClassification.CrossValidate(split.TrainSet, pipeline, numberOfFolds: 5, labelColumnName: "Label");
+
+            //may need to use regularization to reduce overfitting. Works by penalizing the magnitude of model parameters.
+            //** SdcaMaxEntropy already implements regularization into the algorithm
+
 
             //transform data
             var transformedData = model.Transform(split.TestSet);
@@ -62,10 +68,28 @@ namespace EmotionClassification
             var testMetrics = mlContext.MulticlassClassification.Evaluate(transformedData);
 
             Console.WriteLine($"LogLoss is: {testMetrics.LogLoss}");
+            
             Console.WriteLine($"PerClassLogLoss is: {String.Join(" , ", testMetrics.PerClassLogLoss.Select(c => c.ToString()))}");
-            Console.WriteLine($"* MicroAccuracy:    {testMetrics.MicroAccuracy:0.###}");
+            Console.WriteLine($"MicroAccuracy:    {testMetrics.MicroAccuracy:0.###}");
 
             //Predict("Iris-setosa", 0f, 0f, 0f, 0f, 0f, 0f);
+        }
+
+        public void ModelMetrics()
+        {
+            //transform data
+            var transformedData = model.Transform(split.TestSet);
+
+            //Evaluate on transformed data
+            var testMetrics = mlContext.MulticlassClassification.Evaluate(transformedData);
+
+            Console.WriteLine("Following data based on a 20% split from training set.");
+            Console.WriteLine(testMetrics.ConfusionMatrix.GetFormattedConfusionTable());
+            Console.WriteLine($"Average Log-Loss is: {testMetrics.LogLoss}");
+            Console.WriteLine($"Per Class Log-Loss is: {String.Join(" , ", testMetrics.PerClassLogLoss.Select(c => c.ToString()))}");
+            Console.WriteLine($"MicroAccuracy:    {testMetrics.MicroAccuracy:0.###}");
+            Console.WriteLine($"MacroAccuracy:    {testMetrics.MacroAccuracy:0.###}");
+
         }
 
         public void InitPredictor()
--- EmotionClassification/EmotionClassification/Program.cs
+++ EmotionClassification/EmotionClassification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace EmotionClassification
 {
@@ -9,38 +10,34 @@ namespace EmotionClassification
 
         static void Main(string[] args)
         {
-            model = new EmotionModel();
             fExctractor = new FeatureExtraction();
+            model = new EmotionModel(fExctractor.path + "feature_vectors.csv");
 
             Console.WriteLine("Welcome to the Emotion Classifier.");
             while (true)
             {
-                Console.WriteLine("Enter <1> to train model, <2> to test image, <3> to create csv testing data,, <4> to clear console text, <5> to exit ");
+                Console.WriteLine("Enter <0> to train model, Enter <1> to view model evaluation, <2> to test image, <3> to create csv testing data,, <4> to clear console text, <5> to exit ");
                 var input = Console.ReadLine().Trim();
 
+                if (input == "0")
+                {
+                    Console.WriteLine("");
+                    model.TrainModel();
+                    model.SaveModel();
+                    Console.WriteLine("\n");
+                }
                 if (input == "1")
                 {
                     Console.WriteLine("");
-                    model.TrainModel(fExctractor.path + "feature_vectors.csv");
-                    model.SaveModel();
+                    model.LoadModel();
+                    model.ModelMetrics();
                     Console.WriteLine("\n");
                 }
                 else if(input == "2")
                 {
-                    model.LoadModel();
-                    model.InitPredictor();
-
-                    //get feature vectors of all the faces from the input image
-                    float[,] totalVectors = fExctractor.FeatureMaps(fExctractor.path + "test_input.jpg");
-                    for (int i = 0; i < totalVectors.GetLength(0); i++)
-                    {
-                        Console.WriteLine("");
-                        model.Predict("", totalVectors[i, 0], totalVectors[i, 1], totalVectors[i, 2], totalVectors[i, 3], totalVectors[i, 4], totalVectors[i, 5]);
-                        Console.WriteLine("\n");
-                    }
-
+                    PredictAllImages();
                 }
-                 else if(input == "3")
+                else if (input == "3")
                 {
                     Console.WriteLine("");
                     fExctractor.CreateCSV();
@@ -52,8 +49,7 @@ namespace EmotionClassification
                     Console.Clear();
                     Console.WriteLine("Welcome to the Emotion Classifier.");
                 }
-                else if (input == "5")
-                {
+                else if (input == "5") { 
                     Environment.Exit(0);
                 }
                 else
@@ -62,6 +58,37 @@ namespace EmotionClassification
                 }
             }
             
+        }
+
+        //method to predict all images within the tesiImages directory
+        public static void PredictAllImages()
+        {
+            model.LoadModel();
+            model.InitPredictor();
+
+            //get all image files within the test images directory and predict them
+            string testImgPath = fExctractor.path + "testImages";
+            foreach (var file in Directory.GetFiles(testImgPath))
+            {
+                //Console.WriteLine(file);
+                //get feature vectors of all the faces from the input image
+                float[,] totalVectors = fExctractor.FeatureMaps(file);
+
+                if (totalVectors != null)
+                {
+                    //cycle through each face present in the image
+                    for (int i = 0; i < totalVectors.GetLength(0); i++)
+                    {
+                        Console.WriteLine("");
+
+                        string[] filename = file.Split(@"\");
+                        Console.WriteLine(filename[1] + " done.");
+                      
+                        model.Predict("", totalVectors[i, 0], totalVectors[i, 1], totalVectors[i, 2], totalVectors[i, 3], totalVectors[i, 4], totalVectors[i, 5]);
+                        Console.WriteLine("\n");
+                    }
+                }
+            }
         }
     }
 }
